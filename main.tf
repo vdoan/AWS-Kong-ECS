0b35fe5c160ade837ee75577ef280a7014fddb58
nick <ec2-user@ip-172-31-12-154.ap-southeast-2.compute.internal>
2018-07-25 23:30:23 +0000

    feat: sg

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
   
 data "aws_availability_zones" "available" {}
 
+# VPC
 module "vpc" {
   source = "modules/vpc"
 
@@ -21,4 +22,107 @@ module "vpc" {
     Terraform = "true"
     Environment = "dev"
   }
+}
+
+# SGs
+module "ecs_sg" {
+  source      = "modules/sg"
+  name        = "${var.app_name}-ECS-SG"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  # todo elb only
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  ingress_with_cidr_blocks = [
+    {
+      description = "ssh"
+      protocol    = "tcp"
+      from_port   = 22
+      to_port     = 22
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      description = "http"
+      protocol    = "tcp"
+      from_port   = 8000
+      to_port     = 8000
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      description = "https"
+      protocol    = "tcp"
+      from_port   = 8443
+      to_port     = 8443
+      cidr_blocks = "0.0.0.0/0"
+    }
+    # TODO 8001 admin from bastion only
+  ]
+  ingress_with_self = [
+    {
+      description = "kong inter-node comms, tcp"
+      from_port   = 7946
+      to_port     = 7946
+      protocol    = "tcp"
+    },
+    {
+      description = "kong inter-node comms, udp"
+      from_port   = 7946
+      to_port     = 7946
+      protocol    = "udp"
+    }
+  ]
+}
+
+# ECS
+data "aws_ami" "ecs" {
+  most_recent = true
+  filter {
+    name      = "owner-alias"
+    values    = ["amazon"]
+  }
+  filter {
+    name      = "name"
+    values    = ["amzn-ami-*-amazon-ecs-optimized"]
+  }
+}
+
+#module "asg" {
+#  source = "modules/asg"
+#
+#  name = "${var.app_name}-ECS-ASG"
+#
+#  # Launch configuration
+#  lc_name = "${var.app_name}-ECS-LC"
+#
+#  image_id        = "${data.aws_ami.ecs.id}"
+#  instance_type   = "${var.ecs_cluster_instance_type}"
+#  #security_groups = ["sg-12345678"]
+#
+#  # block devices - use defaults
+#
+#  # Auto scaling group
+#  asg_name                  = "${var.app_name}-ECS-ASG"
+#  vpc_zone_identifier       = ["subnet-1235678", "subnet-87654321"]
+#  health_check_type         = "EC2"
+#  min_size                  = 1
+#  max_size                  = 2
+#  desired_capacity          = 1
+#  wait_for_capacity_timeout = 0
+#
+#  tags = [
+#    {
+#      key                 = "Environment"
+#      value               = "dev"
+#      propagate_at_launch = true
+#    },
+#    {
+#      key                 = "Project"
+#      value               = "kong demo"
+#      propagate_at_launch = true
+#    },
+#  ]
+#}
+
+output "ami_id" {
+  value = "${data.aws_ami.ecs.id}"
 }
