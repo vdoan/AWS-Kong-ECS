39cbd9844f7657f8647834026111626e887067c8
nick <ec2-user@ip-172-31-12-154.ap-southeast-2.compute.internal>
2018-07-26 00:06:29 +0000

    feat: module just for ecs iam

---
+++ modules/ecs_cluster_iam/main.tf
@@ -0,0 +1,55 @@
+# IAM roles for ECS
+resource "aws_iam_role" "ecs_role" {
+  name               = "ecs_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_role_policy" "ecs_instance_role_policy" {
+  name     = "ecs_instance_role_policy"
+  policy   = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecs:CreateCluster",
+        "ecs:DeregisterContainerInstance",
+        "ecs:DiscoverPollEndpoint",
+        "ecs:Poll",
+        "ecs:RegisterContainerInstance",
+        "ecs:StartTelemetrySession",
+        "ecs:Submit*",
+        "ecs:StartTask"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+  role     = "${aws_iam_role.ecs_role.id}"
+}
+
+# Instance profile for this role - to be attached to ASG instances
+resource "aws_iam_instance_profile" "ecs" {
+  name = "ecs-instance-profile"
+  path = "/"
+  role = "${aws_iam_role.ecs_role.name}"
+}
+
+output "ecs_instance_profile_id" {
+  value = "${aws_iam_instance_profile.ecs.id}"
+}
