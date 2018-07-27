1169f7731c3ce517853dc1630dfeb15d642a0006
nick <ec2-user@ip-172-31-12-154.ap-southeast-2.compute.internal>
2018-07-27 08:50:33 +0000

    fix: bastion use vpc sgs, not classic

--- bastion.tf
+++ bastion.tf
@@ -41,10 +41,9 @@ resource "aws_instance" "bastion" {
   ami                             = "${data.aws_ami.ubuntu.id}"
   instance_type                   = "${var.bastion_instance_class}"
   key_name                        = "${var.ssh_key_name}"
-  security_groups                 = ["${aws_security_group.bastion.id}"]
+  vpc_security_group_ids          = ["${aws_security_group.bastion.id}"]
   subnet_id                       = "${module.vpc.public_subnets[0]}"
   associate_public_ip_address     = true
-  # vpc_security_group_ids
 
   tags {
     Name = "Bastion"
